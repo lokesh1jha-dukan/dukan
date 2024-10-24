bd8e050464f6e4562495b4cfa2b7d06ec355a2ee
lokeshjha <lokesh1jha@gmai.com>
2024-10-24 23:00:02 +0530

    minor changes

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,9 @@ import { ConfigProps } from "@/types";
 const config: ConfigProps = {
     domainName: process.env.NEXT_PUBLIC_DOMAIN_NAME!,
     appName: process.env.NEXT_PUBLIC_APP_NAME!,
-    jwtSecret: process.env.NEXT_PUBLIC_JWT_SECRET!,
-    directusDomain: process.env.NEXT_PUBLIC_DIRECTUS_DOMAIN!,
-    directusFileDomain: `${process.env.NEXT_PUBLIC_DIRECTUS_DOMAIN!}/assets`,
+    jwtSecret: process.env.JWT_SECRET!,
+    directusDomain: process.env.DIRECTUS_DOMAIN!,
+    directusFileDomain: `${process.env.DIRECTUS_DOMAIN!}/assets`,
     redis: {
         host: process.env.REDIS_HOST!,
         port: parseInt(process.env.REDIS_PORT!),
